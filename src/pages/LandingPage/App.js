fdb6a957fc1c54f99beafb4c17dcdfb934d49ac1
morag <moragmcclenaghan@live.com>
2022-08-05 14:29:21 +0100

    added Register page and css files and png for the page

--- src/pages/LandingPage/App.js
+++ src/pages/LandingPage/App.js
@@ -6,6 +6,8 @@ import LandingImage from "../../assets/LandingImage.png"
 import '../../style/Header.css'
 import '../../style/index.css'
 import Button from 'react-bootstrap/Button';
+import { BrowserRouter as Router, Route, Routes, useNavigate} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 
@@ -32,7 +34,7 @@ function App() {
     />
     <h4>Your journey starts here</h4>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>	
-    <Button variant="signup" size="lg">Sign Up</Button>{' '}
+    <Button variant="signup" size="lg"><Link to="registerpage.js">Sign Up</Link></Button>{' '}
     <p>OR</p>
     <Button variant="login" size="lg">Log in</Button>{' '}
     </div>
