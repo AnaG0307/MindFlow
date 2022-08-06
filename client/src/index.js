1d480da4189d281eba712edb8c3fb771b5b0d285
AdonelleS <adonellesagoe@gmail.com>
2022-08-06 18:13:55 +0100

    FINAL CHANGE!

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "../../pages/landingPage/App";
-import Header from "../../components/header/Header.js";
-import reportWebVitals from "../reportWebVitals";
-import Footer from "../../components/footer/Footer.js";
-import Dashboard from "../../pages/dashboard";
-import Registration from "../../components/login/Registration.js";
-import Login from "../../components/login/Login.js";
-import Period from "../../components/period/Period.js";
+import Home from "./pages/landingPage/App";
+import Header from "./components/header/Header.js";
+import reportWebVitals from "./reportWebVitals";
+import Footer from "./components/footer/Footer.js";
+import Dashboard from "./pages/dashboard/MoodLog.js";
+import Registration from "./components/login/Registration.js";
+import Login from "./components/login/Login.js";
+import Period from "./components/period/Period.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ThemeProvider from "react-bootstrap/ThemeProvider";
 
