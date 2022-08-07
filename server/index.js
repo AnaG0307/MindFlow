c4e9ff72d7dc5e264f9bd016b9b796a0d4e1a858
EditaDol <edita.dolnickova@hotmail.com>
2022-08-07 18:43:25 +0100

    server file updates

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const config =require("./config/index.js");
+const db = require("./config/db.js") ;
+const userRouter = require('./routes/user');
+const authenticate = require("./middleware/authenticate.js") ;
+
+
+
+
+const app = express();
+
+db(config.MONGO_URI, app);
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+
+
+app.get("/", authenticate, async (req, res) => {
+  res.status(200).json(req.user);
+  });
+
+app.use("/api/user", userRouter);
+
+
+app.listen(config.PORT, () =>
+  console.log(`App listening on PORT ${config.PORT}`)
+);
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,31 +1,48 @@
-const express = require("express");
-const cors = require("cors");
-const config =require("./config/index.js");
-const db = require("./config/db.js") ;
-const userRouter = require('./routes/user');
-const authenticate = require("./middleware/authenticate.js") ;
+const http = require('http');
+const app = require('./index');
+require('dotenv').config();
 
-const app = express();
+const normalizePort = val => {
+  const port = parseInt(val, 10);
 
-db(config.MONGO_URI, app);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '4000');
+app.set('port', port);
 
-app.use(cors({ origin: true }));
-app.use(express.json());
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
 
+const server = http.createServer(app);
 
-// get a user from req.user added in the authentication middleware and send the document back
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
 
-app.get("/", authenticate, async (req, res) => {
-  res.status(200).json(req.user);
-  });
-
-
-//routes
-
-app.use("/api/user", userRouter);
-
-//PORT set up
-
-app.listen(config.PORT, () =>
-  console.log(`App listening on PORT ${config.PORT}`)
-);
+server.listen(port);
