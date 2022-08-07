df3e244d3748f4bc902a2b15c45ea1671ded9cfd
morag <moragmcclenaghan@live.com>
2022-08-07 14:22:50 +0100

    created new landing page branch and styled the page elements. also added css sheet for page

--- client/src/pages/landingPage/App.js
+++ client/src/pages/landingPage/App.js
@@ -1,59 +1,60 @@
-import React from "react";
-import "../../App.css";
-import Logo from "../../assets/logo.png";
-import Logo2 from "../../assets/logo2.png";
-import LandingImage from "../../assets/LandingImage.png";
-import "../../style/Header.css";
-import "../../style/index.css";
-import Button from "react-bootstrap/Button";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useNavigate,
-} from "react-router-dom";
-import { Link } from "react-router-dom";
+import React from 'react';
+import '../../App.css'
+import Logo from "../../assets/logo.png"
+import Logo2 from "../../assets/logo2.png"
+import LandingImage from "../../assets/LandingImage.png"
+import '../../style/Header.css'
+import '../../style/index.css'
+import Button from 'react-bootstrap/Button';
+import '../../style/LandingPage.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
+  
   return (
-    <div className="App">
-      <img
-        src={Logo2}
-        alt="MindFlow logo"
-        className="mfl-logo"
-        width="120"
-        height="100"
-      />
-      <h1>MindFlow</h1>
-      <h4>Your wellbeing companion</h4>
-      <img
-        src={LandingImage}
-        alt="MindFlow logo"
-        className="mfl-logo"
-        width="550"
-        height="500"
-      />
-      <h4>Your journey starts here</h4>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <Button variant="signup" size="lg">
-        <Link to="registerpage.js">Sign Up</Link>
-      </Button>{" "}
-      <p>OR</p>
-      <Button variant="login" size="lg">
-        Log in
-      </Button>{" "}
-      <Button variant="period" size="lg">
-        <Link to="/Period">Period Calendar</Link>
-      </Button>{" "}
-    </div>
+    <div className='App'>
+    <Container>
+      <Row>
+        <Col xs={12} lg={6}>
+            <img
+            src={Logo2} 
+            alt= "MindFlow logo" 
+            className="landinglogo"
+            width="120" 
+            height="100"
+            />	
+              <h1 className="landingheader">MindFlow</h1>
+              <h4 className="landingtagline">Your wellbeing companion</h4>
+              <img
+            src={LandingImage} 
+            alt= "MindFlow logo" 
+            className="landingimage"
+            width="550" 
+            height="500"
+            /> 
+        </Col>
+        <Col xs={12} lg={6}>
+          <div className="rhc">
+                <h3 className="landingheadline">Your journey starts here</h3>
+                <text className="landingpararaph">Lorem ipsum dolor sit amet, consectetur adipiscing elit,adipiscing<br/> 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br/>
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris <br/>
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in <br/>
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla <br/>
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa <br/>
+                qui officia deserunt mollit anim id est laborum.</text>	
+                <div className="buttongroup">
+                <Button className="signupbutton" size="med">Sign Up</Button>
+                <h6 className="or">OR</h6>
+                <Button className="loginbutton" size="med">Log in</Button>
+                </div>
+          </div>
+        </Col>
+      </Row>        
+    </Container>
+    </div>  
   );
 }
 
