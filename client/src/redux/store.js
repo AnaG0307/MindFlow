056014efbd00ad9a0646ddfa599e4cee2ad348ea
AnaG0307 <ana.genover@gmail.com>
2022-08-10 15:37:30 +0200

    added period info to Store and created reducers

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slice/authSlice";
-import profileApi from "./slice/profileSlice"
+import profileApi from "./slice/profileSlice";
+import periodReducer from "../pages/periodPage/periodSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-   // [profileApi.reducerPath]: profileApi.reducer
+    // [profileApi.reducerPath]: profileApi.reducer
+    periodInformation: periodReducer,
   },
 
-//middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(profileApi.middleware)
-
-
+  //middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(profileApi.middleware)
 });
