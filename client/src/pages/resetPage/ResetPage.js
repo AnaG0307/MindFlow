918f15b0a26fe607976ba98f6f47bcb22006b18f
AnaG0307 <ana.genover@gmail.com>
2022-08-13 11:56:14 +0200

    deleted unused import inside files in pages folder

--- client/src/pages/resetPage/ResetPage.js
+++ client/src/pages/resetPage/ResetPage.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import '../../App.css'
-import Logo from "../../assets/logo.png"
-import Logo2 from "../../assets/logo2.png"
-import RegisterPage from "../../assets/RegisterPage.png"
-import '../../style/Header.css'
-import '../../style/index.css'
-
-import Reset from "../../components/login/Reset"
-import Sidebar from "../../components/sidebar/Sidebar";
+import React from "react";
+import "../../App.css";
+import "../../style/Header.css";
+import "../../style/index.css";
+import Reset from "../../components/login/Reset";
 
 function ResetPage() {
-
-
   return (
     <div className="App">
-      
       <Reset />
-      
     </div>
-    
   );
 }
 
