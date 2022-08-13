f7a53bee2b0dca724b8bbe28beec06ccceb97291
EditaDol <edita.dolnickova@hotmail.com>
2022-08-13 21:46:35 +0100

    style

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import App from "./app";
-
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 
