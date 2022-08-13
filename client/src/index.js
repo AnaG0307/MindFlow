ca3faa119b0e7f15b6256974702064808e81f1dd
EditaDol <edita.dolnickova@hotmail.com>
2022-08-13 21:46:18 +0100

    style

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-//import { StoreProvider } from "easy-peasy";
-//import store from "./store/store";
 import reportWebVitals from "./reportWebVitals";
 import App from "./app";
 
