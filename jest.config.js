a91b35d8f1d44e18f45f2f075242f6f95c00cbb4
Imane Ziouche <107505051+Imane-zch@users.noreply.github.com>
2022-08-13 19:34:20 +0100

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  transform: {
-    "^.+\\.(ts|tsx)?$": "ts-jest",
-    "^.+\\.(js|jsx)$": "babel-jest",
-  },
-  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(js|jsx)$",
-  moduleFileExtensions: ["js", "json", "jsx"],
-};
