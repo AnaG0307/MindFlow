d2cf80b13f3bdac11818b75504e189e7d8edc7fd
AnaG0307 <ana.genover@gmail.com>
2022-08-14 11:04:00 +0200

    Added path to logo in navbar

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -6,34 +6,30 @@ import React from "react";
 import Logo2 from "../../assets/logo2.png";
 import "../../style/Header.css";
 import "../../style/index.css";
-import { getAuth, signOut} from "firebase/auth";
+import { getAuth, signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
 function Header() {
-  
   const auth = getAuth();
   const navigate = useNavigate();
 
-
-  const handleAnchorClick = (e) => { 
-    
+  const handleAnchorClick = (e) => {
     e.preventDefault();
 
-      signOut(auth) 
+    signOut(auth)
       .then(() => {
-        navigate("/logout")
+        navigate("/logout");
         console.log("user signed out");
-       })
+      })
       .catch((error) => {
-         console.log("error", error);
-     });
-    }
-
+        console.log("error", error);
+      });
+  };
 
   return (
     <Navbar key={false} expand={false} className="mb-3">
       <Container fluid>
-        <Navbar.Brand href="#">
+        <Navbar.Brand href="/">
           <img
             src={Logo2}
             alt="MindFlow logo"
@@ -66,7 +62,7 @@ function Header() {
             <Nav className="justify-content-end flex-grow-1 pe-3">
               <Nav.Link href="/UserProfile">Profile</Nav.Link>
               <Nav.Link href="/settings">Settings</Nav.Link>
-              <Nav.Link onClick = {handleAnchorClick}> Log out</Nav.Link>
+              <Nav.Link onClick={handleAnchorClick}> Log out</Nav.Link>
             </Nav>
           </Offcanvas.Body>
         </Navbar.Offcanvas>
