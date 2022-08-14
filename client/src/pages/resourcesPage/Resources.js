2fa6f124443c140f4698fb66000594ef0dcf9f19
AdonelleS <adonellesagoe@gmail.com>
2022-08-14 18:51:30 +0100

    cleaned up code

--- client/src/pages/resourcesPage/Resources.js
+++ client/src/pages/resourcesPage/Resources.js
@@ -14,42 +14,44 @@ import Mentalhealth from "../../assets/mental-health.png"
 
 function ResourcesPage() {
   return (
-    <div className="ResourcesPage">
+
+  <div className="ResourcesPage">
     <Sidebar />
-      <Container>
-        <Row>
-          <h3 className="firstTitle">Your mental wellbeing matters</h3>
-          <h3 className="resourcesHeading"> If you are in need of help please see some links and helpful guides below </h3> 
-          <Col xs={12} lg={10} style={{ paddingLeft: 0, marginLeft: 500}}>
-            <div className="rightcols">
-              <Resources />
-            </div>
-          </Col>
-          <Col xs={10} lg={8} style={{ paddingLeft: 0, marginLeft: 500 }}>
-            <div>
-              <img src={ResourcesImage} alt= "2 ladies" className="resources-image"/>
-            </div>
+    <Container>
+    <Row>
+      <h3 className="firstTitle">Your mental wellbeing matters</h3>
+      <h3 className="resourcesHeading"> If you are in need of help please see some links and helpful guides below </h3> 
+      <Col xs={12} lg={10} style={{ paddingLeft: 0, marginLeft: 500}}>
+        <div className="rightcols">
+        <Resources />
+        </div>
+      </Col>
+  
+    <Col xs={10} lg={8} style={{ paddingLeft: 0, marginLeft: 500 }}>
+      <div>
+        <img src={ResourcesImage} alt= "2 ladies" className="resources-image"/>
+        </div>
         <div className="Resources-table">
         <div className="Resources">
         <h3 className="paragraph"> <a href="https://www.mentalhealth.org.uk/explore-mental-health/blogs" target="_blank" rel="noreferrer"><img src={Article} alt= "article" className="article-image"/></a>Helpful Articles</h3>
         <p className="pparagraph"> Helpful articles, blogs and personal experiences and tips from young people and our MindFlow team.
-       </p>
-       </div>
+        </p>
+      </div>
      <div className="Resources">
        <h3 className="paragraph"><a href="https://www.nhs.uk/nhs-services/mental-health-services/" target="_blank" rel="noreferrer"><img src={Mentalhealth} alt= "mental-health" className="mental-image"/></a>Mental Health</h3>
-         <p className="pparagraph"> Start or join a conversation with our friendly MindFlow community.
+        <p className="pparagraph"> Start or join a conversation with our friendly MindFlow community.
         </p>
-         </div>
+      </div>
          <div className="Resources">
-        <h3 className="paragraph"><a href="https://www.counselling-directory.org.uk/" target="_blank" rel="noreferrer"><img src={Friend} alt= "friend" className="friend-image"/></a>Chat to a specialist</h3>
-        <p className="pparagraph"> Find some local therapists in your area to receive extra counselling.
-        </p>   
-          </div>
-          </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+          <h3 className="paragraph"><a href="https://www.counselling-directory.org.uk/" target="_blank" rel="noreferrer"><img src={Friend} alt= "friend" className="friend-image"/></a>Chat to a specialist</h3>
+          <p className="pparagraph"> Find some local therapists in your area to receive extra counselling.
+          </p>   
+        </div>
+      </div>
+    </Col>
+    </Row>
+    </Container>
+  </div>
   );
 }
 
