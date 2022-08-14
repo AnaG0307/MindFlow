4aa8953c693ff8c4d191bd6265dcea9b5f24576e
AdonelleS <adonellesagoe@gmail.com>
2022-08-14 17:59:07 +0100

    final resource page

--- client/src/pages/resourcesPage/Resources.js
+++ client/src/pages/resourcesPage/Resources.js
@@ -20,29 +20,29 @@ function ResourcesPage() {
         <Row>
           <h3 className="firstTitle">Your mental wellbeing matters</h3>
           <h3 className="resourcesHeading"> If you are in need of help please see some links and helpful guides below </h3> 
-          <Col xs={12} lg={8} style={{ paddingLeft: 0 }}>
+          <Col xs={12} lg={10} style={{ paddingLeft: 0, marginLeft: 500}}>
             <div className="rightcols">
               <Resources />
             </div>
           </Col>
-          <Col xs={10} lg={4} style={{ paddingLeft: 0, marginLeft: 500 }}>
+          <Col xs={10} lg={8} style={{ paddingLeft: 0, marginLeft: 500 }}>
             <div>
               <img src={ResourcesImage} alt= "2 ladies" className="resources-image"/>
             </div>
         <div className="Resources-table">
         <div className="Resources">
         <h3 className="paragraph"> <a href="https://www.mentalhealth.org.uk/explore-mental-health/blogs" target="_blank" rel="noreferrer"><img src={Article} alt= "article" className="article-image"/></a>Helpful Articles</h3>
-        <p> Helpful articles, blogs and personal experiences and tips from young people and our MindFlow team.
+        <p className="pparagraph"> Helpful articles, blogs and personal experiences and tips from young people and our MindFlow team.
        </p>
        </div>
      <div className="Resources">
        <h3 className="paragraph"><a href="https://www.nhs.uk/nhs-services/mental-health-services/" target="_blank" rel="noreferrer"><img src={Mentalhealth} alt= "mental-health" className="mental-image"/></a>Mental Health</h3>
-         <p> Start or join a conversation with our friendly MindFlow community.
+         <p className="pparagraph"> Start or join a conversation with our friendly MindFlow community.
         </p>
          </div>
          <div className="Resources">
         <h3 className="paragraph"><a href="https://www.counselling-directory.org.uk/" target="_blank" rel="noreferrer"><img src={Friend} alt= "friend" className="friend-image"/></a>Chat to a specialist</h3>
-        <p> Find some local therapists in your area to receive extra counselling.
+        <p className="pparagraph"> Find some local therapists in your area to receive extra counselling.
         </p>   
           </div>
           </div>
