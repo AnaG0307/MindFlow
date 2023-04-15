8c13f81f7f1ef084fa131fa1c20bc8d391e32561
AnaG0307 <ana.genover@gmail.com>
2023-04-15 18:14:06 +0100

    Fixed hamburguer menu not expanding properly

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,61 +1,47 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Offcanvas from "react-bootstrap/Offcanvas";
-import React from "react";
-import Logo2 from "../../assets/logo2.png";
-import "../../style/Header.css";
-import "../../style/index.css";
-import { getAuth, signOut } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
+import Offcanvas from "react-bootstrap/Offcanvas"
+import React from "react"
+import Logo2 from "../../assets/logo2.png"
+import "../../style/Header.css"
+import "../../style/index.css"
+import { getAuth, signOut } from "firebase/auth"
+import { useNavigate } from "react-router-dom"
 
 function Header() {
-  const auth = getAuth();
-  const navigate = useNavigate();
+  const auth = getAuth()
+  const navigate = useNavigate()
 
   const handleAnchorClick = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     signOut(auth)
       .then(() => {
-        navigate("/logout");
-        console.log("user signed out");
+        navigate("/logout")
+        console.log("user signed out")
       })
       .catch((error) => {
-        console.log("error", error);
-      });
-  };
+        console.log("error", error)
+      })
+  }
 
   return (
     <Navbar key={false} expand={false} className="mb-3">
       <Container fluid>
         <Navbar.Brand href="/">
-          <img
-            src={Logo2}
-            alt="MindFlow logo"
-            className="mfl-logo"
-            width="49"
-            height="40"
-          />
+          <LogoImage></LogoImage>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-false`} />
         <Navbar.Offcanvas
           id={`offcanvasNavbar-expand-false`}
           aria-labelledby={`offcanvasNavbarLabel-expand-false`}
           placement="end"
-          className="headerbar"
-          style={{ fontSize: "20px" }}
         >
           <Offcanvas.Header closeButton>
             <Offcanvas.Title id={`offcanvasNavheaderbarbarLabel-expand-false`}>
               MindFlow
-              <img
-                src={Logo2}
-                alt="MindFlow logo"
-                className="mfl-logo"
-                width="40"
-                height="30"
-              />
+              <LogoImage></LogoImage>
             </Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
@@ -68,7 +54,21 @@ function Header() {
         </Navbar.Offcanvas>
       </Container>
     </Navbar>
-  );
+  )
 }
 
-export default Header;
+const LogoImage = () => {
+  return (
+    <>
+      <img
+        src={Logo2}
+        alt="MindFlow logo"
+        className="mfl-logo"
+        width="49"
+        height="40"
+      />
+    </>
+  )
+}
+
+export default Header
